7914a65e8de268d8116e2bccc2dcc0c82721e4df
jmbarriga1 <69165872+jmbarriga1@users.noreply.github.com>
2020-11-05 13:08:09 -0300

    Add files via upload

--- caso_4.py
+++ caso_4.py
@@ -6,7 +6,7 @@ from matplotlib import cm
 a = 1.     #alto del dominio
 b = 0.5     #ancho del dominio
 
-Nx = 30    #numero de intervalos en X
+Nx = 15    #numero de intervalos en X
 Ny = 30    #numero de intervalos en Y
 
 dx = b/Nx  #discretizacion espacial en x
@@ -43,12 +43,12 @@ def imshowbien(u):
     xTicks_N = arange(0, Nx+1, 3)
     yTicks_N = arange(0, Ny+1, 3)
     xTicks =[coords(i,0)[0] for i in xTicks_N]
-    yTicks =[coords(0,i)[0] for i in yTicks_N]
+    yTicks =[coords(0,i)[1] for i in yTicks_N]
     xTicks_Text = ["{0:.2f}".format(tick) for tick in xTicks]
     yTicks_Text = ["{0:.2f}".format(tick) for tick in yTicks]
     xticks(xTicks_N,xTicks_Text, rotation='vertical')
     yticks(yTicks_N,yTicks_Text)
-    margins(0,2)
+    margins(0.2)
     subplots_adjust(bottom=0.15)
     
     
